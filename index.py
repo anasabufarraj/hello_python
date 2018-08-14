6c0fbb5028e779e1ddd175aeda1ad5ff11b46f30
Anas Abu Farraj <anas.r.abufarraj@gmail.com>
2018-08-14 12:36:34 +0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,36 @@
+#! /usr/local/bin/python3.6
+# Copyright 2018. Anas Abu Farraj.
+
+
+class Egg:
+    # Initial Constructors
+    def __init__(self, kind="Fried"):
+        self.kind = kind
+
+    def what_kind(self):
+        return self.kind
+
+
+def main():
+    fried = Egg()  # Use default value "Fried"
+    scrambled = Egg("Scrambled")
+    print(fried.what_kind())
+    print(scrambled.what_kind())
+
+
+if __name__ == '__main__':
+    main()
+
+
+# TODO: Study this example
+class Door:
+    def open(self):
+        print('Hello stranger')
+
+
+def knock_door():
+    a_door = Door()
+    Door.open(a_door)
+
+
+knock_door()
