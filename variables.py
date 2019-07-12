c43c7d78a2ecc00bdc61ec00f21ae81d49a9fe2b
Anas Abu Farraj <anas.r.abufarraj@gmail.com>
2019-07-12 12:05:07 +0300

    backup

--- variables.py
+++ variables.py
@@ -1,8 +1,6 @@
 #!/usr/local/bin/python3
 # Copyright 2019. Anas Abu Farraj
 
-prefix = 'No.'
-number = 42
+is_smaller = 1 < 2
 
-print(prefix + number)
-print(prefix + str(number))
+print(is_smaller)  # true
