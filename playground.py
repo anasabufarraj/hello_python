96795d34e27e65cbb5145deca17bf00c02499762
Anas Abu Farraj <anas.r.abufarraj@gmail.com>
2019-08-16 23:33:53 +0300

    backup

--- playground.py
+++ playground.py
@@ -1,23 +1,12 @@
 #!./venv/bin/python3.7
 # Copyright 2019. Anas Abu Farraj
 
+try:
+    a = int('hello')
+except ValueError:
+    print('Oops! Invalid number.')
+finally:
+    print('Hello, will be printed anyway.')
 
-class BaseClass:
-    def __init__(self, company='Some', made='Car!'):
-        self._company = company
-        self._made = made
-
-    def company(self):
-        return self._company
-
-    def made(self):
-        return self._made
-
-
-class ChildClass(BaseClass):
-    pass
-
-
-car = ChildClass('BMW', '2002')
-
-print(car.company(), car.made())
+# Oops! Invalid number.
+# Hello, will be printed anyway.
