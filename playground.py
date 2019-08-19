c1f691b32b41211a578403c6b340bf277c4277f2
Anas Abu Farraj <anas.r.abufarraj@gmail.com>
2019-08-19 17:02:19 +0300

    Backup

--- playground.py
+++ playground.py
@@ -1,12 +1,6 @@
 #!./venv/bin/python3.7
 # Copyright 2019. Anas Abu Farraj
 
-try:
-    a = int('hello')
-except ValueError:
-    print('Oops! Invalid number.')
-finally:
-    print('Hello, will be printed anyway.')
+animals = ['cat', 'dog', 'mouse', 'rooster']
 
-# Oops! Invalid number.
-# Hello, will be printed anyway.
+print(';'.join(animals))  # cat;dog;mouse;rooster
