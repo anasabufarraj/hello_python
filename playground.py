cc7f501416dbe2ded1ca0fc19d7ca4fb4808fa55
Anas Abu Farraj <anas.r.abufarraj@gmail.com>
2019-09-13 17:19:21 +0300

    Backup project

--- playground.py
+++ playground.py
@@ -1,10 +1,26 @@
 #!./venv/bin/python3.7
 # Copyright 2019. Anas Abu Farraj
 
+# yes_votes = 42_572_654
+# no_votes = 43_132_459
+# percentage_yes_votes = yes_votes / (yes_votes + no_votes)
+# percentage_no_votes = no_votes / (yes_votes + no_votes)
 
-def my_add(a: int, b: int) -> int:
-    return a + b
+# print(f'{yes_votes:,} YES Votes {percentage_yes_votes:.1%}')
+# print(f'{no_votes:,} NO Votes {percentage_no_votes:.1%}')
+# -------------------------------------------------------
 
+import json
 
-def greeting(name: str) -> str:
-    return name
+text_data = {
+    "name": "hello_jquery",
+    "version": "1.0.0",
+    "description": "learning jQuery",
+    "main": "index.html"
+}
+
+# Writing text data to file:
+json.dump(text_data,
+          open('files/data_dump.json', 'wt'),
+          indent=4,
+          sort_keys=True)
