7f5ee277c037c7df56fb1244055e1bfb97ecf54a
Anas Abu Farraj <anas.r.abufarraj@gmail.com>
2019-09-19 18:42:51 +0300

    Backup project

--- playground.py
+++ playground.py
@@ -1,26 +1,10 @@
 #!./venv/bin/python3.7
 # Copyright 2019. Anas Abu Farraj
 
-# yes_votes = 42_572_654
-# no_votes = 43_132_459
-# percentage_yes_votes = yes_votes / (yes_votes + no_votes)
-# percentage_no_votes = no_votes / (yes_votes + no_votes)
+from string import Template
 
-# print(f'{yes_votes:,} YES Votes {percentage_yes_votes:.1%}')
-# print(f'{no_votes:,} NO Votes {percentage_no_votes:.1%}')
-# -------------------------------------------------------
+t = Template('${sender} send ${amount} to ${reciever}')
 
-import json
+print(t.substitute(sender='Jonny', amount='$40', reciever='Peter'))
 
-text_data = {
-    "name": "hello_jquery",
-    "version": "1.0.0",
-    "description": "learning jQuery",
-    "main": "index.html"
-}
-
-# Writing text data to file:
-json.dump(text_data,
-          open('files/data_dump.json', 'wt'),
-          indent=4,
-          sort_keys=True)
+# Jonny send $40 to Peter
