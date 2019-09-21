959f18c38d306484f34d1eb1a3586add0b3f9012
Anas Abu Farraj <anas.r.abufarraj@gmail.com>
2019-09-21 14:31:50 +0300

    Backup project

---
+++ app.dart
@@ -0,0 +1,8 @@
+// app.dart
+// Copyright 2019. Anas Abu Farraj
+// Learning Dart
+
+// Dart forces the usage of functions
+main() {
+  print('Hello')
+}
---
+++ app.py
@@ -0,0 +1,3 @@
+#!./venv/bin/python3.7
+# Copyright 2019. Anas Abu Farraj
+# Learning Python
