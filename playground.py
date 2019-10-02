6038d55a8b03250dd7a30277bd064374a1bd0158
Anas Abu Farraj <anas.r.abufarraj@gmail.com>
2019-10-02 13:53:17 +0300

    Backup project

--- playground.py
+++ playground.py
@@ -2,17 +2,7 @@
 # Copyright 2019. Anas Abu Farraj
 # Learning Python
 
-template = """
-From: <{from_email}>
-To: <{to_email}>
-Subject: {subject}
-
-{message}
-"""
-
-print(
-    template.format(from_email="sender@domain.com",
-                    to_email="recipient@domain.com",
-                    subject="Hello!",
-                    message="Here's some mail for you. "
-                    " Hope you enjoy the message!"))
+import rook
+# rest of your code go here
+rook.start(
+    token='8461b7cb90b10d756dce48777d679d3e1fa39f12602161434948d45bd837b08d')
